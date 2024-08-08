a4e41a1d553283c998688b92d36edcadccb1cb74
lahiri <lahiriyatin01@gmail.com>
2024-08-09 03:29:48 +0530

    merged temp-branch to Develop

--- reactbasic/src/customer/components/HomeCarousel/MainCarousel.jsx
+++ reactbasic/src/customer/components/HomeCarousel/MainCarousel.jsx
@@ -4,7 +4,7 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 import carouselImages from './CarouselImages';
 import './MainCarousel.css'; // Import the custom CSS file
 
-// Create items for the carousel
+
 const items = carouselImages.map((image, index) => (
   <div key={index} className="carousel-item">
     <img src={image} alt={`Carousel ${index + 1}`} />
