d588f3178d526cefed74c70eb400e7d3ff24e6b7
Orimepax <117072653+Orimepax@users.noreply.github.com>
2024-08-10 21:59:44 +0530

    Revert "added nav and image changes"

--- reactbasic/src/customer/components/HomeSectionCard/CardImages.js
+++
@@ -1,5 +0,0 @@
-const images = require.context('../../../../public/images/HomeSectionCardImages', false, /\.(png|jpg)$/);
-
-const cardImages = images.keys().map(images);
-
-export default cardImages;
